0db6c5cfc835798fb25d08b913ca8b23a459d4dd
aamontford <amamontford1@gmail.com>
2025-05-12 11:26:26 +0000

    created employee atructure

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db'); 
+const employeeRoutes = require('./routes/employeeRoutes');
+
 
 dotenv.config();
 connectDB();
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json()); 
+app.use('/api/employees', employeeRoutes);
+
 app.listen(port, () => {
-  console.log(`✅ Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ routes/employeeRoutes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const Employee = require('../models/employee');
+
+// Create an employee
+router.post('/', async (req, res) => {
+  try {
+    const newEmployee = new Employee(req.body);
+    await newEmployee.save();
+    res.status(201).json(newEmployee);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get all employees
+router.get('/', async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Get an employee
+router.get('/:id', async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) 
+        return res.status(404).json({ error: 'Employee not found' });
+    res.json(employee);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Update employee
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await Employee.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updated) 
+        return res.status(404).json({ error: 'Employee not found' });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Delete employee
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Employee.findByIdAndDelete(req.params.id);
+    if (!deleted) 
+        return res.status(404).json({ error: 'Employee not found' });
+    res.json({ message: 'Employee deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
